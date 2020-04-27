9795b6cea680dbe89cdbdefe475106e7f6c22dc9
ypranab <ypranab11@gmail.com>
2020-04-27 22:08:01 +0600

    deploy user page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import User from './components/Users/User';
+
+function App() {
+  return (
+    <div className="App">
+      <User></User>
+    </div>
+  );
+}
+
+export default App;
